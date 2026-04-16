c72252a06f82a80ff2f086f8d3919b508b6050cb
Jerryco-1 <lawrencesombreno@gmail.com>
2026-04-16 11:29:30 +0800

    removed email

--- Simple_GUI/src/Frames/Register.java
+++ Simple_GUI/src/Frames/Register.java
@@ -28,12 +28,9 @@ public class Register extends javax.swing.JFrame {
         Lusername2 = new javax.swing.JLabel();
         tuser = new javax.swing.JTextField();
         line1 = new javax.swing.JLabel();
-        Lemail = new javax.swing.JLabel();
         tuser1 = new javax.swing.JTextField();
         line3 = new javax.swing.JLabel();
         Lpassword = new javax.swing.JLabel();
-        jPassfield = new javax.swing.JPasswordField();
-        line2 = new javax.swing.JLabel();
         showpass = new javax.swing.JCheckBox();
         jLabel4 = new javax.swing.JLabel();
         BttnRecruit = new javax.swing.JLabel();
@@ -51,12 +48,12 @@ public class Register extends javax.swing.JFrame {
         jLabel2.setFont(new java.awt.Font("Rockwell Extra Bold", 1, 25)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(254, 250, 175));
         jLabel2.setText("Join The Corps");
-        Main.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 40, -1, -1));
+        Main.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 40, -1, -1));
 
         Lusername2.setFont(new java.awt.Font("Rockwell", 1, 14)); // NOI18N
         Lusername2.setForeground(new java.awt.Color(252, 211, 2));
         Lusername2.setText("Username");
-        Main.add(Lusername2, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 130, -1, -1));
+        Main.add(Lusername2, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 120, -1, -1));
 
         tuser.setBackground(new java.awt.Color(51, 51, 51));
         tuser.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
@@ -70,17 +67,12 @@ public class Register extends javax.swing.JFrame {
                 tuserActionPerformed(evt);
             }
         });
-        Main.add(tuser, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 120, 330, 40));
+        Main.add(tuser, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 140, 330, 40));
 
         line1.setFont(new java.awt.Font("Rockwell", 1, 14)); // NOI18N
         line1.setForeground(new java.awt.Color(255, 255, 255));
         line1.setText("______________________________________________");
-        Main.add(line1, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 150, -1, -1));
-
-        Lemail.setFont(new java.awt.Font("Rockwell", 1, 14)); // NOI18N
-        Lemail.setForeground(new java.awt.Color(252, 211, 2));
-        Lemail.setText("Email");
-        Main.add(Lemail, new org.netbeans.lib.awtextra.AbsoluteConstraints(568, 210, -1, -1));
+        Main.add(line1, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 170, -1, -1));
 
         tuser1.setBackground(new java.awt.Color(51, 51, 51));
         tuser1.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
@@ -91,33 +83,17 @@ public class Register extends javax.swing.JFrame {
                 tuser1ActionPerformed(evt);
             }
         });
-        Main.add(tuser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 200, 330, 40));
+        Main.add(tuser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 230, 330, 40));
 
         line3.setFont(new java.awt.Font("Rockwell", 1, 14)); // NOI18N
         line3.setForeground(new java.awt.Color(255, 255, 255));
         line3.setText("______________________________________________");
-        Main.add(line3, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 230, -1, -1));
+        Main.add(line3, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 260, -1, -1));
 
         Lpassword.setFont(new java.awt.Font("Rockwell", 1, 14)); // NOI18N
         Lpassword.setForeground(new java.awt.Color(252, 211, 2));
         Lpassword.setText("Breathing Style");
-        Main.add(Lpassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 360, -1, 40));
-
-        jPassfield.setBackground(new java.awt.Color(51, 51, 51));
-        jPassfield.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
-        jPassfield.setForeground(new java.awt.Color(255, 255, 255));
-        jPassfield.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jPassfield.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jPassfieldActionPerformed(evt);
-            }
-        });
-        Main.add(jPassfield, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 270, 330, 40));
-
-        line2.setFont(new java.awt.Font("Rockwell", 1, 14)); // NOI18N
-        line2.setForeground(new java.awt.Color(255, 255, 255));
-        line2.setText("______________________________________________");
-        Main.add(line2, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 300, -1, -1));
+        Main.add(Lpassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 330, -1, 20));
 
         showpass.setFont(new java.awt.Font("Rockwell", 1, 12)); // NOI18N
         showpass.setForeground(new java.awt.Color(252, 211, 2));
@@ -127,12 +103,12 @@ public class Register extends javax.swing.JFrame {
                 showpassActionPerformed(evt);
             }
         });
-        Main.add(showpass, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 330, 137, -1));
+        Main.add(showpass, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 290, 137, -1));
 
         jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 153));
         jLabel4.setText("Already a Slayer?");
-        Main.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 530, 110, -1));
+        Main.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 530, 110, -1));
 
         BttnRecruit.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         BttnRecruit.setForeground(new java.awt.Color(255, 255, 153));
@@ -142,7 +118,7 @@ public class Register extends javax.swing.JFrame {
                 BttnRecruitMouseClicked(evt);
             }
         });
-        Main.add(BttnRecruit, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 530, 50, -1));
+        Main.add(BttnRecruit, new org.netbeans.lib.awtextra.AbsoluteConstraints(810, 530, 50, -1));
 
         jComboBox1.setBackground(new java.awt.Color(51, 51, 51));
         jComboBox1.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
@@ -154,12 +130,12 @@ public class Register extends javax.swing.JFrame {
                 jComboBox1ActionPerformed(evt);
             }
         });
-        Main.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 370, 330, -1));
+        Main.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 350, 330, -1));
 
         Lpassword1.setFont(new java.awt.Font("Rockwell", 1, 14)); // NOI18N
         Lpassword1.setForeground(new java.awt.Color(252, 211, 2));
         Lpassword1.setText("Password");
-        Main.add(Lpassword1, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 280, -1, -1));
+        Main.add(Lpassword1, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 210, -1, -1));
 
         jLabel9.setForeground(new java.awt.Color(204, 255, 255));
         jLabel9.setText(" © 2026 Demon Slayer - All Rights Reserved");
@@ -177,7 +153,7 @@ public class Register extends javax.swing.JFrame {
                 bttnenlistActionPerformed(evt);
             }
         });
-        Main.add(bttnenlist, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 440, 330, 40));
+        Main.add(bttnenlist, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 440, 330, 40));
 
         Nezukobg.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frames/nezukobg.jpg"))); // NOI18N
         Main.add(Nezukobg, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1000, 600));
@@ -199,10 +175,6 @@ public class Register extends javax.swing.JFrame {
     private void tuserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tuserActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_tuserActionPerformed
-
-    private void jPassfieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPassfieldActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jPassfieldActionPerformed
 
     private void showpassActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_showpassActionPerformed
         // TODO add your handling code here:
@@ -265,7 +237,6 @@ public class Register extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel BttnRecruit;
-    private javax.swing.JLabel Lemail;
     private javax.swing.JLabel Lpassword;
     private javax.swing.JLabel Lpassword1;
     private javax.swing.JLabel Lusername2;
@@ -276,9 +247,7 @@ public class Register extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel9;
-    private javax.swing.JPasswordField jPassfield;
     private javax.swing.JLabel line1;
-    private javax.swing.JLabel line2;
     private javax.swing.JLabel line3;
     private javax.swing.JCheckBox showpass;
     private javax.swing.JTextField tuser;
